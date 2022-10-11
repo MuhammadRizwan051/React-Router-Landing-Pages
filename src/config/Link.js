ef5430ee9bc530902cda2ac6b65055b3e4124fb0
Your Name <you@example.com>
2022-10-11 14:37:13 +0500

    Landing Pages with React Router

---
+++ src/config/Link.js
@@ -0,0 +1,24 @@
+// import React from "react";
+// import { BrowserRouter as Link } from "react-router-dom";
+
+// function Abc() {
+//   return (
+//     <>
+//       <div>
+//         <ul style={{ display: "flex", listStyle: "none" }}>
+//           <li>
+//             <Link to="home">A</Link>
+//           </li>
+//           <li>
+//             <Link to="about">B</Link>
+//           </li>
+//           <li>
+//             <Link to="profile">C</Link>
+//           </li>
+//         </ul>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Abc;
